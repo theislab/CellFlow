191a6be9eb953d7e791c8fd48c36e2d2686614f6
selmanozleyen <syozleyen@gmail.com>
2025-04-23 14:49:35 +0200

    update states

--- src/cellflow/model/_cellflow.py
+++ src/cellflow/model/_cellflow.py
@@ -480,7 +480,8 @@ class CellFlow:
             )
         else:
             raise NotImplementedError(f"Solver must be an instance of OTFlowMatching or GENOT, got {type(self.solver)}")
-        self._trainer = CellFlowTrainer(solver=self.solver, condition_keys= self.train_data.data_manager.condition_keys)  # type: ignore[arg-type]
+        condition_keys = sorted(layers_before_pool.keys())
+        self._trainer = CellFlowTrainer(solver=self.solver, condition_keys=condition_keys)  # type: ignore[arg-type]
 
     def train(
         self,
