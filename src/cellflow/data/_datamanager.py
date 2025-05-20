7332e5cce78d2ed54bc2708b371accf1a9ad93eb
selmanozleyen <syozleyen@gmail.com>
2025-05-20 13:16:50 +0200

    add tests

--- src/cellflow/data/_datamanager.py
+++ src/cellflow/data/_datamanager.py
@@ -5,8 +5,6 @@ import anndata
 import dask
 import dask.dataframe as dd
 import dask.delayed
-import jax
-import jax.numpy as jnp
 import numpy as np
 import pandas as pd
 import scipy.sparse as sp
@@ -353,13 +351,13 @@ class DataManager:
         else:
             split_covariates_mask = None
             perturbation_covariates_mask = None
-            control_mask = jnp.ones((len(covariate_data),))
+            control_mask = np.ones((len(covariate_data),))
 
-        condition_data: dict[str, list[jnp.ndarray]] = (
+        condition_data: dict[str, list[np.ndarray]] = (
             {i: [] for i in self._covar_to_idx.keys()} if self.is_conditional else {}
         )
 
-        control_to_perturbation: dict[int, ArrayLike] = {}
+        control_to_perturbation: dict[int, np.ndarray] = {}
         split_idx_to_covariates: dict[int, tuple[Any]] = {}
         perturbation_idx_to_covariates: dict[int, tuple[Any]] = {}
         perturbation_idx_to_id: dict[int, Any] = {}
@@ -426,10 +424,10 @@ class DataManager:
         # convert outputs to jax arrays
         if self.is_conditional:
             for pert_cov, emb in condition_data.items():
-                condition_data[pert_cov] = jnp.array(emb)
-        split_covariates_mask = jnp.asarray(split_covariates_mask) if split_covariates_mask is not None else None
+                condition_data[pert_cov] = np.array(emb)
+        split_covariates_mask = np.asarray(split_covariates_mask) if split_covariates_mask is not None else None
         perturbation_covariates_mask = (
-            jnp.asarray(perturbation_covariates_mask) if perturbation_covariates_mask is not None else None
+            np.asarray(perturbation_covariates_mask) if perturbation_covariates_mask is not None else None
         )
         return ReturnData(
             split_covariates_mask=split_covariates_mask,
@@ -454,7 +452,7 @@ class DataManager:
         max_combination_length: int,
         linked_perturb_covars: dict[str, dict[Any, Any]],
         sample_covariates: Sequence[str],
-    ) -> dict[str, jax.Array]:
+    ) -> dict[str, np.ndarray]:
         """Get perturbation covariates embeddings.
 
         Parameters
@@ -490,7 +488,7 @@ class DataManager:
 
         primary_group, primary_covars = next(iter(perturb_covariates.items()))
 
-        perturb_covar_emb: dict[str, list[jax.Array]] = {group: [] for group in perturb_covariates}
+        perturb_covar_emb: dict[str, list[np.ndarray]] = {group: [] for group in perturb_covariates}
         for primary_cov in primary_covars:
             value = condition_data[primary_cov]
             cov_name = value if is_categorical else primary_cov
@@ -498,9 +496,9 @@ class DataManager:
                 rep_key = covariate_reps[primary_group]
                 if cov_name not in rep_dict[rep_key]:
                     raise ValueError(f"Representation for '{cov_name}' not found in `adata.uns['{rep_key}']`.")
-                prim_arr = jnp.asarray(rep_dict[rep_key][cov_name])
+                prim_arr = np.asarray(rep_dict[rep_key][cov_name])
             else:
-                prim_arr = jnp.asarray(
+                prim_arr = np.asarray(
                     primary_one_hot_encoder.transform(  # type: ignore[union-attr]
                         np.array(cov_name).reshape(-1, 1)
                     )
@@ -516,7 +514,7 @@ class DataManager:
                 linked_group, linked_cov = list(linked_covar)
 
                 if linked_cov is None:
-                    linked_arr = jnp.full((1, 1), null_value)
+                    linked_arr = np.full((1, 1), null_value)
                     linked_arr = check_shape_fn(linked_arr)
                     perturb_covar_emb[linked_group].append(linked_arr)
                     continue
@@ -527,23 +525,23 @@ class DataManager:
                     rep_key = covariate_reps[linked_group]
                     if cov_name not in rep_dict[rep_key]:
                         raise ValueError(f"Representation for '{cov_name}' not found in `adata.uns['{linked_group}']`.")
-                    linked_arr = jnp.asarray(rep_dict[rep_key][cov_name])
+                    linked_arr = np.asarray(rep_dict[rep_key][cov_name])
                 else:
-                    linked_arr = jnp.asarray(condition_data[linked_cov])
+                    linked_arr = np.asarray(condition_data[linked_cov])
 
                 linked_arr = check_shape_fn(linked_arr)
                 perturb_covar_emb[linked_group].append(linked_arr)
 
         perturb_covar_emb = {
             k: pad_to_max_length_fn(
-                jnp.concatenate(v, axis=0),
+                np.concatenate(v, axis=0),
                 max_combination_length,
                 null_value,
             )
             for k, v in perturb_covar_emb.items()
         }
 
-        sample_covar_emb: dict[str, jax.Array] = {}
+        sample_covar_emb: dict[str, np.ndarray] = {}
         for sample_cov in sample_covariates:
             value = condition_data[sample_cov]
             if sample_cov in covariate_reps:
@@ -551,12 +549,12 @@ class DataManager:
 
                 if value not in rep_dict[rep_key]:
                     raise ValueError(f"Representation for '{value}' not found in `adata.uns['{sample_cov}']`.")
-                cov_arr = jnp.asarray(rep_dict[rep_key][value])
+                cov_arr = np.asarray(rep_dict[rep_key][value])
             else:
-                cov_arr = jnp.asarray(value)
+                cov_arr = np.asarray(value)
 
             cov_arr = check_shape_fn(cov_arr)
-            sample_covar_emb[sample_cov] = jnp.tile(cov_arr, (max_combination_length, 1))
+            sample_covar_emb[sample_cov] = np.tile(cov_arr, (max_combination_length, 1))
 
         return perturb_covar_emb | sample_covar_emb
 
@@ -615,7 +613,7 @@ class DataManager:
         perturbation_idx_to_id: dict[int, Any] = {}
         split_covariates_mask = None
         perturbation_covariates_mask = None
-        condition_data: dict[str, list[jnp.ndarray]] = (
+        condition_data: dict[str, list[np.ndarray]] = (
             {i: [] for i in self._covar_to_idx.keys()} if self.is_conditional else {}
         )
         perturb_covariates = {k: _to_list(v) for k, v in self._perturbation_covariates.items()}
@@ -747,8 +745,8 @@ class DataManager:
         control_to_perturbation = {k: np.array(v, dtype=np.int32) for k, v in control_to_perturbation.items()}
         df.set_index("cell_index", inplace=True)
         df = df.reindex(covariate_data.index)
-        split_covariates_mask = jnp.asarray(df["split_covariates_mask"].values, dtype=jnp.int32)
-        perturbation_covariates_mask = jnp.asarray(df["perturbation_covariates_mask"].values, dtype=jnp.int32)
+        split_covariates_mask = np.asarray(df["split_covariates_mask"].values, dtype=np.int32)
+        perturbation_covariates_mask = np.asarray(df["perturbation_covariates_mask"].values, dtype=np.int32)
 
         # Create delayed tasks for each condition
         delayed_results = []
@@ -779,7 +777,7 @@ class DataManager:
                 condition_data[pert_cov].append(emb)
 
         for pert_cov, emb in condition_data.items():
-            condition_data[pert_cov] = jnp.array(emb)
+            condition_data[pert_cov] = np.array(emb)
 
         res = ReturnData(
             split_covariates_mask=split_covariates_mask,
@@ -812,21 +810,20 @@ class DataManager:
         self,
         adata: anndata.AnnData,
         sample_rep: str | None = None,
-        device: str = "cpu",
-    ) -> jax.Array:
+    ) -> np.ndarray:
         sample_rep = self._sample_rep if sample_rep is None else sample_rep
         if sample_rep == "X":
             sample_rep = adata.X
             if isinstance(sample_rep, sp.csr_matrix):
-                return jnp.asarray(sample_rep.toarray(), device=jax.devices("cpu")[0])
+                return np.asarray(sample_rep.toarray())
             else:
-                return jnp.asarray(sample_rep, device=jax.devices("cpu")[0])
+                return np.asarray(sample_rep)
         if isinstance(self._sample_rep, str):
             if self._sample_rep not in adata.obsm:
                 raise KeyError(f"Sample representation '{self._sample_rep}' not found in `adata.obsm`.")
-            return jnp.asarray(adata.obsm[self._sample_rep], device=jax.devices("cpu")[0])
+            return np.asarray(adata.obsm[self._sample_rep])
         attr, key = next(iter(sample_rep.items()))  # type: ignore[union-attr]
-        return jax.device_put(jnp.asarray(getattr(adata, attr)[key]), device=jax.devices("cpu")[0])
+        return np.asarray(getattr(adata, attr)[key])
 
     def _verify_control_data(self, adata: anndata.AnnData | None) -> None:
         if adata is None:
@@ -865,7 +862,7 @@ class DataManager:
     ) -> tuple[ArrayLike, dict[int, tuple[Any]], ArrayLike]:
         filter_dict = dict(zip(self.split_covariates, split_combination, strict=False))
         split_cov_mask = (covariate_data[list(filter_dict.keys())] == list(filter_dict.values())).all(axis=1)
-        mask = jnp.array(control_mask * split_cov_mask).astype(bool)
+        mask = np.array(control_mask * split_cov_mask).astype(bool)
         split_covariates_mask[mask] = src_counter
         split_idx_to_covariates[src_counter] = tuple(split_combination)
         return split_covariates_mask, split_idx_to_covariates, split_cov_mask
@@ -875,7 +872,7 @@ class DataManager:
     ) -> tuple[ArrayLike, dict[int, tuple[Any]]]:
         # here we assume that adata only contains source cells
         if len(self.split_covariates) == 0:
-            return jnp.full((len(adata),), 0, dtype=jnp.int32), {}
+            return np.full((len(adata),), 0, dtype=np.int32), {}
         split_covariates_mask = np.full((len(adata),), -1, dtype=np.int32)
         split_idx_to_covariates: dict[int, Any] = {}
         src_counter = 0
@@ -886,14 +883,14 @@ class DataManager:
                 split_covariates_mask=split_covariates_mask,
                 split_combination=split_combination,
                 split_idx_to_covariates=split_idx_to_covariates,
-                control_mask=jnp.ones((adata.n_obs,)),
+                control_mask=np.ones((adata.n_obs,)),
                 src_counter=src_counter,
             )
 
             if (split_covariates_mask == split_covariates_mask_previous).all():
                 raise ValueError(f"No cells found in `adata` for split covariates {split_combination}.")
             src_counter += 1
-        return jnp.asarray(split_covariates_mask), split_idx_to_covariates
+        return np.asarray(split_covariates_mask), split_idx_to_covariates
 
     @staticmethod
     def _verify_perturbation_covariates(data: dict[str, Sequence[str]] | None) -> dict[str, list[str]]:
@@ -1098,14 +1095,14 @@ class DataManager:
     @staticmethod
     def _check_shape(arr: float | ArrayLike) -> ArrayLike:
         if not hasattr(arr, "shape") or len(arr.shape) == 0:
-            return jnp.ones((1, 1)) * arr
+            return np.ones((1, 1)) * arr
         if arr.ndim == 1:  # type: ignore[union-attr]
-            return jnp.expand_dims(arr, 0)
+            return np.expand_dims(arr, 0)
         elif arr.ndim == 2:  # type: ignore[union-attr]
             if arr.shape[0] == 1:
                 return arr  # type: ignore[return-value]
             if arr.shape[1] == 1:
-                return jnp.transpose(arr)
+                return np.transpose(arr)
             raise ValueError(
                 "Condition representation has an unexpected shape. Should be (1, n_features) or (n_features, )."
             )
@@ -1125,10 +1122,10 @@ class DataManager:
         return covar_to_idx
 
     @staticmethod
-    def _pad_to_max_length(arr: jax.Array, max_combination_length: int, null_value: Any) -> jax.Array:
+    def _pad_to_max_length(arr: np.ndarray, max_combination_length: int, null_value: Any) -> np.ndarray:
         if arr.shape[0] < max_combination_length:
-            null_arr = jnp.full((max_combination_length - arr.shape[0], arr.shape[1]), null_value)
-            arr = jnp.concatenate([arr, null_arr], axis=0)
+            null_arr = np.full((max_combination_length - arr.shape[0], arr.shape[1]), null_value)
+            arr = np.concatenate([arr, null_arr], axis=0)
         return arr
 
     def _get_perturbation_covariates(
@@ -1136,10 +1133,10 @@ class DataManager:
         condition_data: pd.DataFrame,
         rep_dict: dict[str, dict[str, ArrayLike]],
         perturb_covariates: Any,  # TODO: check if we can save as attribtue
-    ) -> dict[str, jax.Array]:
+    ) -> dict[str, np.ndarray]:
         primary_group, primary_covars = next(iter(perturb_covariates.items()))
 
-        perturb_covar_emb: dict[str, list[jax.Array]] = {group: [] for group in perturb_covariates}
+        perturb_covar_emb: dict[str, list[np.ndarray]] = {group: [] for group in perturb_covariates}
         for primary_cov in primary_covars:
             value = condition_data[primary_cov]
             cov_name = value if self.is_categorical else primary_cov
@@ -1147,9 +1144,9 @@ class DataManager:
                 rep_key = self._covariate_reps[primary_group]
                 if cov_name not in rep_dict[rep_key]:
                     raise ValueError(f"Representation for '{cov_name}' not found in `adata.uns['{rep_key}']`.")
-                prim_arr = jnp.asarray(rep_dict[rep_key][cov_name])
+                prim_arr = np.asarray(rep_dict[rep_key][cov_name])
             else:
-                prim_arr = jnp.asarray(
+                prim_arr = np.asarray(
                     self.primary_one_hot_encoder.transform(  # type: ignore[union-attr]
                         np.array(cov_name).reshape(-1, 1)
                     )
@@ -1165,7 +1162,7 @@ class DataManager:
                 linked_group, linked_cov = list(linked_covar)
 
                 if linked_cov is None:
-                    linked_arr = jnp.full((1, 1), self._null_value)
+                    linked_arr = np.full((1, 1), self._null_value)
                     linked_arr = self._check_shape(linked_arr)
                     perturb_covar_emb[linked_group].append(linked_arr)
                     continue
@@ -1176,35 +1173,35 @@ class DataManager:
                     rep_key = self._covariate_reps[linked_group]
                     if cov_name not in rep_dict[rep_key]:
                         raise ValueError(f"Representation for '{cov_name}' not found in `adata.uns['{linked_group}']`.")
-                    linked_arr = jnp.asarray(rep_dict[rep_key][cov_name])
+                    linked_arr = np.asarray(rep_dict[rep_key][cov_name])
                 else:
-                    linked_arr = jnp.asarray(condition_data[linked_cov])
+                    linked_arr = np.asarray(condition_data[linked_cov])
 
                 linked_arr = self._check_shape(linked_arr)
                 perturb_covar_emb[linked_group].append(linked_arr)
 
         perturb_covar_emb = {
             k: self._pad_to_max_length(
-                jnp.concatenate(v, axis=0),
+                np.concatenate(v, axis=0),
                 self._max_combination_length,
                 self._null_value,
             )
             for k, v in perturb_covar_emb.items()
         }
 
-        sample_covar_emb: dict[str, jax.Array] = {}
+        sample_covar_emb: dict[str, np.ndarray] = {}
         for sample_cov in self._sample_covariates:
             value = condition_data[sample_cov]
             if sample_cov in self._covariate_reps:
                 rep_key = self._covariate_reps[sample_cov]
                 if value not in rep_dict[rep_key]:
                     raise ValueError(f"Representation for '{value}' not found in `adata.uns['{sample_cov}']`.")
-                cov_arr = jnp.asarray(rep_dict[rep_key][value])
+                cov_arr = np.asarray(rep_dict[rep_key][value])
             else:
-                cov_arr = jnp.asarray(value)
+                cov_arr = np.asarray(value)
 
             cov_arr = self._check_shape(cov_arr)
-            sample_covar_emb[sample_cov] = jnp.tile(cov_arr, (self._max_combination_length, 1))
+            sample_covar_emb[sample_cov] = np.tile(cov_arr, (self._max_combination_length, 1))
 
         return perturb_covar_emb | sample_covar_emb
 
