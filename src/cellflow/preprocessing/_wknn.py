f7ef2cd4e8458f84f805d17ea548b914baa2dfbb
joschif <jonas.simon.fleck@gmail.com>
2025-08-29 16:45:26 +0200

    fix: int conversion befor sparse

--- src/cellflow/preprocessing/_wknn.py
+++ src/cellflow/preprocessing/_wknn.py
@@ -135,7 +135,7 @@ def transfer_labels(
 
     wknn = ref_adata.uns[wknn_key]
     clusters_onehot = pd.get_dummies(ref_adata.obs[label_key].astype(str))
-    clusters_mat = sparse.csr_matrix(clusters_onehot > 0).astype(int)
+    clusters_mat = sparse.csr_matrix((clusters_onehot > 0).astype(int))
 
     scores = wknn @ clusters_mat
     label_indices = np.array((scores).argmax(1)).flatten()
